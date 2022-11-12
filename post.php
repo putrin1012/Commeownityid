6a3209c5e00e3fc9dd749eda3e772c0e45da66cb
astrobila <astro.bila@gmail.com>
2022-11-12 20:42:51 +0700

    no message

--- post.php
+++ post.php
@@ -1,11 +1,18 @@
 <div id="post", style="max-width: 500px;background-color: #FFFFFF">
   <div>
+    <div class="row">
+      <div class="col-2">
     <img src="img/logo_dark.png" style="width: 75px;margin-right: 4px;border: solid;border-radius:20%">
   </div>
+  <div class="col-10">
     <div style="font-weigth: bold;color: #405d9b">Test Post</div>
+  </div>
+  </div>
+  </div>
     Ini cuma buat ngetest doang
   <br/><br/>
 
-  <a href="">Like</a>.<a href="">Comment</a>.<span style="color: #999;">Date</a>
+  <a>Date</a>
+  <a href="">Like</a><a href=""> Comment </a><span style="color: #999;">
 
   </div>
