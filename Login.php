5b76933e8a31aade40d4a95d0fdda182d88cb2aa
Indira <indira6166@gmail.com>
2022-11-13 06:12:47 +0700

    Login & Register

---
+++ Login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Welcome to CommeownityID!</title>
+        <meta charset="UTF-8">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link rel="stylesheet" type="text/css"
+          href="main.css" title="Default Styles" media="screen">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <div class="limiter">
+            <div class="container-login100">
+                <div class="wrap-login100">
+
+                <div class="login100-pic js-tilt" data-tilt>
+					          <img src="img/logo_dark.png" alt="IMG">
+				        </div>
+
+                    <form class="login100-form validate-form">
+                        <span class="login100-form-title"> Login </span>
+                        <div class="wrap-input100 validate-input" data-validate="valid email is required: meow@paw.com">
+                            <input class="input100" type="email" name="email" placeholder="Email">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="fa fa-envelope" aria-hidden="true">
+                                </i>
+                            </span>
+                        </div>
+                        <div class="wrap-input100 validate-input" data-validate="Password is required">
+                            <input class="input100" type="password" name="password" placeholder="Password">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="fa fa-lock" aria-hidden="true">
+                                </i>
+                            </span>
+                        </div>
+                        <div class="container-login100-form-btn">
+                            <button class="login100-form-btn">Login</button>
+                        </div>
+                        <div class="text-center p-t-12">
+                            <span class="txt1">Don't have any account?</span>
+                            <a class="txt2" href="Register.php"> Register here meow~ </a>
+                        </div>
+                        <!-- <div class="text-center p-t-136">
+                            <a class="txt2" href="#">
+                                "Register"
+                                <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true">
+                                </i>
+                            </a>
+                        </div> -->
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script src="login.js"></script>
+    </body>
+</html>
+
+
---
+++ Register.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset'"UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Register</title>
+</head>
+<body>
+    <form class="form-inline" name="Registration Form" action="" method="get">
+        <h3>Username</h3>
+        <input type="text" id="username" />
+        <h3>Email</h3>
+        <input type="email" id="userEmail" />
+        <br />
+        <br />
+        <!-- <label class="my-1 mr-2" for="inlineFormCustomSelectLocation">Location</label> -->
+        <h3>Location</h3>
+        <select class="custom-select my-1 mr-sm-2" id="CustomSelectLocation">
+            <option selected>Lokasi anda...</option>
+            <option value="1">Aceh</option>
+            <option value="2">Bali</option>
+            <option value="3">Bangka Belitung</option>
+            <option value="4">Banten</option>
+            <option value="5">Bengkulu</option>
+            <option value="6">Daerah Istimewa Yogyakarta</option>
+            <option value="7">Gorontalo</option>
+            <option value="8">Jakarta</option>
+            <option value="9">Jambi</option>
+            <option value="10">Jawa Barat</option>
+            <option value="11">Jawa Tengah</option>
+            <option value="12">Jawa Timur</option>
+            <option value="13">Kalimantan Barat</option>
+            <option value="14">Kalimantan Selatan</option>
+            <option value="15">Kalimantan Tengah</option>
+            <option value="16">Kalimantan Timur</option>
+            <option value="17">Kalimantan Utara</option>
+            <option value="18">Kepulauan Riau</option>
+            <option value="19">Lampung</option>
+            <option value="20">Maluku Utara</option>
+            <option value="21">Maluku</option>
+            <option value="23">Nusa Tenggara Barat</option>
+            <option value="24">Nusa Tenggara Timur</option>
+            <option value="25">Papua Barat</option>
+            <option value="26">Papua</option>
+            <option value="27">Riau</option>
+            <option value="28">Sulawesi Selatan</option>
+            <option value="29">Sulawesi Tengah</option>
+            <option value="30">Sulawesi Tenggara</option>
+            <option value="31">Sulawesi Utara</option>
+            <option value="32">Sumatra Barat</option>
+            <option value="33">Sumatra Selatan</option>
+            <option value="34">Sumatra Utara</option>
+        </select>
+        <h3>Password</h3>
+        <input type="text" id="userPassword" />
+        <br />
+        <br />
+        <button type="submit">Register Now</button>
+    </form>
+</body>
+</html>
