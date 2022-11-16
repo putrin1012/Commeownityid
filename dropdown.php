cf3dfbbaa24cfe52567ffc3ecb7fd82307e12b91
putrin1012 <putrin@student.telkomuniversity.ac.id>
2022-11-17 06:55:34 +0700

    no message

--- dropdown.php
+++ dropdown.php
@@ -1,10 +1,8 @@
 <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown button
-    </button>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-    </ul>
+    <select class="btn btn-secondary dropdown-toggle" type="button" name="PostCategory" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" placeholder="Post Category">
+          <option>Kategori Post</option>
+          <option value="Open Adopt">Open Adopt</option>
+          <option value="Need Adopter">Need Adopter</option>
+          <option value="Miscellanous">Miscellanous</option>
+    </select>
 </div>
