7cc665fb63111ad648111922ebce7bccbe97952c
putrin1012 <putrin@student.telkomuniversity.ac.id>
2022-11-16 20:25:16 +0700

    no message

--- NewPost.php
+++ NewPost.php
@@ -53,21 +53,34 @@
   <div class="container bootstrap snippets bootdeys">
       <div class="col-md-7 col-md-offset-2">
           <div class="panel">
+
+
               <div class="panel-body">
-          		<textarea class="form-control" rows="2" placeholder="Write something here..."></textarea>
-          		<div class="mar-top clearfix">
-              </br>
-                <?php include("tagsInput.php");?>
-              </br>
-          			<button class="btn btn-sm btn-primary pull-right" style="background-color:#6C452D;border-color:#6C452D;width:100px;" type="submit"><i class="fa fa-pencil fa-fw"></i> Post</button>
-                <label>
-                  <a class="btn btn-trans btn-icon fa fa-video-camera add-tooltip" data-original-title="Add Video" data-toggle="tooltip"></a>
-                  <input type="file" name="myImage" accept="video/*" style="display:none;"/>
-                </label>
-                <label>
-                  <a class="btn btn-trans btn-icon fa fa-camera add-tooltip" data-original-title="Add Photo" data-toggle="tooltip"></a>
-                  <input type="file" name="myImage" accept="image/png, image/gif, image/jpeg" style="display:none;"/>
-                </label>
+              <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown button
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>
+            </br>
+            		<textarea class="form-control" rows="2" placeholder="Write something here..."></textarea>
+            		<div class="mar-top clearfix">
+                </br>
+                  <?php include("tagsInput.php");?>
+                </br>
+            			<button class="btn btn-sm btn-primary pull-right" style="background-color:#6C452D;border-color:#6C452D;width:100px;" type="submit"><i class="fa fa-pencil fa-fw"></i> Post</button>
+                  <label>
+                    <a class="btn btn-trans btn-icon fa fa-video-camera add-tooltip" data-original-title="Add Video" data-toggle="tooltip"></a>
+                    <input type="file" name="myImage" accept="video/*" style="display:none;"/>
+                  </label>
+                  <label>
+                    <a class="btn btn-trans btn-icon fa fa-camera add-tooltip" data-original-title="Add Photo" data-toggle="tooltip"></a>
+                    <input type="file" name="myImage" accept="image/png, image/gif, image/jpeg" style="display:none;"/>
+                  </label>
           			<a class="btn btn-trans btn-icon fa fa-file add-tooltip" href="#" data-original-title="Add File" data-toggle="tooltip"></a>
           		</div>
           	</div>
