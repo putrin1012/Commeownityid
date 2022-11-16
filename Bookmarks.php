f560b71a7d57aaa4cb14ae9bfb1bd4a85e5937ab
putrin1012 <putrin@student.telkomuniversity.ac.id>
2022-11-16 22:40:34 +0700

    no message

--- Bookmarks.php
+++ Bookmarks.php
@@ -8,8 +8,12 @@
      </div>
     <div class="col-8">
     </br></br></br>
-    <?php include("tagsInput.php");?>
-  </br>
+    <div class="col" style="margin-left:25px;width:1020px;">
+      <?php include("tagsInput.php");?>
+      </br>
+      <button class="btn btn-sm btn-primary pull-right" style="background-color:#6C452D;border-color:#6C452D;width:100px;" type="submit"><i class="fa fa-search fa-fw"></i> Search</button>
+    </div>
+  </br></br></br>
     <?php
         for($i = 0; $i < 10; $i++){
           include("post.php");
