f5baf8250d7bbdc14c32f5dffdac345f6689acbf
putrin1012 <putrin@student.telkomuniversity.ac.id>
2022-11-18 04:33:17 +0700

    no message

--- articleThumbnail.php
+++ articleThumbnail.php
@@ -8,37 +8,21 @@
 
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
                   <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+
                   </div>
                   <div class="carousel-inner">
                     <div class="carousel-item active">
                       <img src="img/tesCarousel.jpg" class="d-block w-100" alt="..." style="width:640px;height:360px">
                     </div>
-                    <div class="carousel-item">
-                      <img src="img/tesCarousel_2.jpg" class="d-block w-100" alt="..."style="width:640px;height:360px">
-                    </div>
-                    <div class="carousel-item">
-                      <img src="img/tesCarousel_3.jpg" class="d-block w-100" alt="..."style="width:640px;height:360px">
-                    </div>
+
                   </div>
-                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                  </button>
-                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                  </button>
-              </br>
-                <p><?php echo $data['textContent']??''; ?></p>
-                <div>
-                  <a class="badge badge-primary" href="#" style="background-color:#6C452D;">Tag1</a>
-                  <a class="badge badge-primary" href="#" style="background-color:#6C452D;">Tag2</a>
-                  <a class="badge badge-primary" href="#" style="background-color:#6C452D;">Tag3</a>
-                  <a class="badge badge-primary" href="#" style="background-color:#6C452D;">Tag4</a>
                 </div>
+
+              <a href="articlePage.php" style="font-size:40px;">Article Title</a>
+
+                <p><?php //echo $data['textContent']??''; ?>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur arcu nunc, tempus et rhoncus ut, porta ac tellus. Curabitur a purus congue mi blandit pretium vitae et felis. Vivamus tempor convallis ante a ullamcorper. Nunc hendrerit est urna, ut rhoncus nulla tincidunt a. Aenean feugiat porta nulla efficitur dapibus. Quisque et posuere arcu. In hac habitasse platea dic<a href="articlePage.php"> Read More...</a></p>
+
+
                 <div class="stats">
                     <a href="#" class="btn btn-default stat-item">
                         <i class="fa fa-thumbs-up icon"></i>2
