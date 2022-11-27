0484dec0b6f49559f409bcd817ddcc73c2a7ffa1
putrin1012 <putrin@student.telkomuniversity.ac.id>
2022-11-27 07:43:51 +0700

    ngikutin tutor yutub yang reading posts

--- Class/User.php
+++ Class/User.php
@@ -1,5 +1,13 @@
 <?php
 class User {
-  
+  public function getData($id){
+    $query = "SELECT * FROM users WHERE ID = '$$id' limit 1";
+    $DB = new database();
+    $result = $DB->read($query);
+
+    if($result) {
+      $row 
+    }
+  }
 }
  ?>
