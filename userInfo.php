50e81db624697db768855746d6db5424903a027b
unknown <indahrani@student.telkomuniversity.ac.id>
2022-12-29 23:14:09 +0700

    profile&sidebar

--- userInfo.php
+++ userInfo.php
@@ -6,38 +6,39 @@
             <div class="col">
                 <div class="card card-style1 border-0">
                     <div class="card-body p-1-9 p-sm-2-3 p-md-6 p-lg-7">
-                        <div class="row align-items-center">
-                          <div class="bg-white shadow rounded overflow-hidden">
-                            <div class="upper">
-                             <img src="img/yy.png" class="img-fluid">
+                        <div class="row align-items-center">  
+                            <div class="row align-items-center">
+                           
                                 <div class="media align-items-end profile-header">
                                     <div class="profile mr-3"><img src="img/oren.png" style="display:block; margin:auto;" width="140" class="rounded-circle img-thumbnail"></div>
                                     <a href="#" class="btn btn-dark btn-sm btn-block">Edit profile</a>                                    
-                                </div>
-                            </div>
-                                <ul class="list-unstyled mb-1-9">
-                                <h4 class="h4 text-white mb-0">John mark Doe Kyzer</h4>
-                                <p>
+                                </div>                          
+                                    <ul class="list-unstyled mb-1-9">
+                                    <h3 font face="Courier New" color="dark" size="3">John mark Doe Kyzer</h3> 
+                                    <div> 
+                                        <button type="button" class="btn btn-outline-primary btn-floating">
+                                            <i class="fa fa-facebook fa-1x"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-outline-primary btn-floating">
+                                            <i class="fa fa-pinterest fa-1x"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-outline-primary btn-floating">
+                                            <i class="fa fa-instagram fa-1x"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-outline-primary btn-floating">
+                                            <i class="fa fa-twitter fa-1x"></i>
+                                        </button>
+                                    </div>
                                     <strong>HoomanCAT</strong>
                                     <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Username:</span>Soma Kyo</li>
                                     <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Lokasi:</span>Jawa Barat</li>
-                                    <!-- <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Email:</span> edith@mail.com</li> -->
+                                        <!-- <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Email:</span> edith@mail.com</li> -->
                                     <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Kontak:</span> <a href="wa.me/" style="font-color='black';"> Whatsapp (Kyo)</span></li>
-                                    <!-- <li class="display-28"><span class="display-26 text-secondary me-2 font-weight-600">Phone:</span> 507 - 541 - 4567</li> -->
-                                </ul>
-                                <div class="social-buttons mt-5"> 
-                                    <button class="neo-button"><i class="fa fa-facebook fa-1x"></i> </button> 
-                                    <button class="neo-button"><i class="fa fa-pinterest fa-1x"></i> </button> 
-                                    <button class="neo-button"><i class="fa fa-instagram fa-1x"></i> </button>
-                                    <button class="neo-button"><i class="fa fa-twitter fa-1x"></i> </button>
-                                </div>
-                                <div class="profile mt-5">
+                                        <!-- <li class="display-28"><span class="display-26 text-secondary me-2 font-weight-600">Phone:</span> 507 - 541 - 4567</li> -->   
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-
         </div>
     </div>
